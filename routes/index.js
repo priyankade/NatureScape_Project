6bf0f8cfe0b7e2621d2a4731fd40a9ad5e60a7df
Priyanka De <p.priyankade@gmail.com>
2022-08-19 16:07:38 -0400

    routing fixed with activityTable

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 const userRoutes = require("./users");
 const privateRoutes = require('./private');
 const homeRoutes = require("./activities");
-const activityTableRoutes = require("./activityTable");
-
 
 const constructorMethod = (app) => {
   app.use("/users", userRoutes);
   app.use('/private', privateRoutes);
-  app.use(" ", homeRoutes);
-  app.use("/",homeRoutes);
+  //app.use(" ", homeRoutes);
+  
+  
   app.use("/search",homeRoutes);
   app.use("/addActivity",homeRoutes);
   app.use("/login",userRoutes);
   app.use("/signup",userRoutes);
   //   app.use("/reviews", reviewsRoute);
+  app.use("/",homeRoutes);
 
   app.use("*", (req, res) => {
     res.status(404).json({ error: "this route is not being used" });
