f3bde11282df07b0ef7d9fca6a53a6243ef99817
Priyanka De <p.priyankade@gmail.com>
2022-08-20 23:20:40 -0400

    fixed seed.js for date

---
+++ routes/events.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const router = express.Router();
+const validate = require('../validation')
+const activitiesData = require('../data/activities');
+const activitiesTableData = require('../data/activityTable');
+const xss = require('xss');
+
+router.get('/addEvent', async (req, res) => {
+    console.log('[addEvent]');
+    if (req.session.user) {
+        res.render('display/addEvent');
+        return;
+    }
+    else {
+        errormessage = {
+            className: "User not logged in",
+            message: "User needs to log in to create Event",
+            hasErrors: "Error",
+            title: "Error"
+        }
+        res.status(401).render('display/error', errormessage);
+        //return res.redirect('/users/login');
+        return;
+    }
+});
+
+router.post('/createEvent', async (req, res) => {
+    console.log('[createEvent]');
+    if (req.session.user) {
+        try {
+            let activityName = xss(req.body.activityName);
+            var validatedActivity = validate.checkActivity(activityName);
+            let EventLocation = xss(req.body.location);
+            var validatedLocation = validate.checkStringWithSpaces(EventLocation, "location");
+            let EventCity = xss(req.body.city);
+            var validatedCity = validate.checkString(EventCity, "city");
+            let EventState = xss(req.body.state);
+            var validatedState = validate.checkState(EventState, "state");
+            let EventDate = xss(req.body.date);
+            var validatedDate = validate.checkDateforFutureActivities(EventDate, "date");
+            let EventOrganizer = xss(req.body.organizer);
+            var validatedOrganizer = validate.checkStringWithSpaces(EventOrganizer, "organizer");
+            let EventExpertise = xss(req.body.expertise);
+            var validatedExpertise = validate.checkExpertise(EventExpertise, "expertise");
+            let EventPrice = xss(req.body.price);
+            var validatedPrice = validate.checkIsProperNumber(EventPrice, "price");
+
+            console.log("expertise is ", validatedExpertise);
+            console.log("price is ", validatedPrice);
+
+            let checkdup = await validate.checkDuplicateEvent(validatedActivity, validatedLocation, validatedCity, validatedState, validatedDate, validatedOrganizer, validatedExpertise, validatedPrice);
+            if ("hasErrors" in checkdup) {
+                throw 'Event already exists';
+            }
+        }
+        catch (error) {
+            errormessage = {
+                className: "Cannot add Event",
+                message: error,
+                hasErrors: "Error",
+                title: "Error"
+            }
+            res.status(400).render("display/addEvent", {
+                activityName: req.body.activityName,
+                location: req.body.location,
+                city: req.body.city,
+                state: req.body.state,
+                date: req.body.date,
+                organizer: req.body.organizer,
+                expertise: req.body.expertise,
+                price: req.body.price,
+                title: "Create Event",
+                error: error,
+            });
+            return;
+        }
+        checkEventCreated = activitiesTableData.createactivityTable(validatedActivity, validatedLocation, validatedCity, validatedState, validatedDate, validatedOrganizer, validatedExpertise, validatedPrice);
+        if ("hasErrors" in checkEventCreated) {
+            errormessage = {
+                className: "Could not add Event",
+                message: "could not add Event",
+                hasErrors: "True",
+                title: "Error"
+            }
+                res.status(400).render("display/addEvent", {
+                    location: req.body.location,
+                    city: req.body.city,
+                    state: req.body.state,
+                    date: req.body.date,
+                    organizer: req.body.organizer,
+                    expertise: req.body.expertise,
+                    price: req.body.price,
+                    title: "Create Event",
+                    error: errormessage,
+                });
+            
+           // res.status(400).render('display/error', "could not add Event");
+            return;
+        }
+        res.status(200).send("Successfully inserted Event");
+    }
+
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,19 @@
 const userRoutes = require("./users");
 const privateRoutes = require('./private');
 const homeRoutes = require("./activities");
+const eventRoutes = require("./events");
 
 const constructorMethod = (app) => {
   app.use("/users", userRoutes);
   app.use('/private', privateRoutes);
   //app.use(" ", homeRoutes);
   
-  
-  app.use("/search",homeRoutes);
-  app.use("/addActivity",homeRoutes);
+  app.use("/events",eventRoutes);
   app.use("/login",userRoutes);
   app.use("/signup",userRoutes);
   //   app.use("/reviews", reviewsRoute);
+  app.use("/search",homeRoutes);
+  app.use("/addActivity",homeRoutes);
   app.use("/",homeRoutes);
 
   app.use("*", (req, res) => {
