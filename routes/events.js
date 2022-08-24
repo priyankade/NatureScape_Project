1c812a865bcd34a939d14bbb7125b44487c80bef
sdutt3 <98142391+sdutt3@users.noreply.github.com>
2022-08-24 18:49:22 -0400

    Handle spaces in handlebars to add event (#55)

--- routes/events.js
+++ routes/events.js
@@ -33,40 +33,38 @@ router.post('/addEvent', async (req, res) => {
 router.post('/createEvent', async (req, res) => {
     console.log('POST [createEvent]');
     if (req.session.user) {
-        console.log(req.body);
         try {
             let activityName = xss(req.body.activityName);
-            var validatedActivity = validate.checkActivity(activityName);
+            let validatedActivity = validate.checkActivity(activityName);
             let overview = xss(req.body.overview);
-            var validatedOverview = validate.checkDescription(overview);
+            let validatedOverview = validate.checkDescription(overview);
             let EventLocation = xss(req.body.location);
-            var validatedLocation = validate.checkStringWithSpaces(EventLocation, "location");
+            let validatedLocation = validate.checkStringWithSpaces(EventLocation, "location");
             let EventCity = xss(req.body.city);
-            var validatedCity = validate.checkStringWithSpaces(EventCity, "city");
+            let validatedCity = validate.checkStringWithSpaces(EventCity, "city");
             let EventState = xss(req.body.state);
-            var validatedState = validate.checkState(EventState, "state");
+            let validatedState = validate.checkState(EventState, "state");
             let EventDate = xss(req.body.date);
-            var validatedDate = validate.checkDateforFutureActivities(EventDate, "date");
+            let validatedDate = validate.checkDateforFutureActivities(EventDate, "date");
             let EventOrganizer = xss(req.body.organizer);
-            var validatedOrganizer = validate.checkStringWithSpaces(EventOrganizer, "organizer");
+            let validatedOrganizer = validate.checkStringWithSpaces(EventOrganizer, "organizer");
             let EventExpertise = xss(req.body.expertise);
-            var validatedExpertise = validate.checkExpertise(EventExpertise, "expertise");
+            let validatedExpertise = validate.checkExpertise(EventExpertise, "expertise");
             let EventPrice = xss(req.body.price);
-            var validatedPrice = validate.checkIsProperNumber(EventPrice, "price");
+            let validatedPrice = validate.checkIsProperNumber(EventPrice, "price");
 
             //FAQ
             let question1 = xss(req.body.question1);
-            var validatedQuestion1 = validate.checkStringWithSpaces(question1, "question1");
+            let validatedQuestion1 = validate.checkStringWithSpaces(question1, "question1");
             let answer1 = xss(req.body.answer1);
-            var validatedAnswer1 = validate.checkStringWithSpaces(answer1, "answer1");
+            let validatedAnswer1 = validate.checkStringWithSpaces(answer1, "answer1");
             let question2 = xss(req.body.question2);
-            var validatedQuestion2 = validate.checkStringWithSpaces(question2, "question2");
+            let validatedQuestion2 = validate.checkStringWithSpaces(question2, "question2");
             let answer2 = xss(req.body.answer2);
-            var validatedAnswer2 = validate.checkStringWithSpaces(answer2, "answer2");
+            let validatedAnswer2 = validate.checkStringWithSpaces(answer2, "answer2");
             //adding organizer email to event
             let username = req.session.user;
             let userDetails = await userData.getUserByUsername(username);
-            console.log(userDetails);
             let organizerEmail = userDetails.email;
             validate.checkEmail(organizerEmail);
 
